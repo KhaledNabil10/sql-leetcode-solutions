8b26482d0b8bbba8f4fa508b9502216efd998a44
Khaled Nabil <153304420+KhaledNabil10@users.noreply.github.com>
2026-06-29 15:59:36 +0300

    Add SQL query for Article Views I problem

---
+++ SQL_50/1148_article_views_i.sql
@@ -0,0 +1,8 @@
+-- Problem: 1148. Article Views I
+-- Difficulty: Easy
+-- Link: https://leetcode.com/problems/article-views-i/
+
+SELECT DISTINCT author_id AS id
+FROM views
+WHERE author_id = viewer_id
+ORDER BY author_id;

d40c60c0d9ee2a9e7f94cf04ff18994bab17f95a
Khaled Nabil <153304420+KhaledNabil10@users.noreply.github.com>
2026-06-29 16:12:06 +0300

    Create SQL query for replacing employee ID
    Add SQL query to replace employee ID with unique identifier

---
+++ SQL_50/1378_replace_employee_id.sql
@@ -0,0 +1,8 @@
+-- Problem: 1378. Replace Employee ID With The Unique Identifier
+-- Difficulty: Easy
+-- Link: https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier/
+
+SELECT eu.unique_id, e.name
+FROM Employees e
+LEFT JOIN EmployeeUNI eu
+    ON e.id = eu.id;

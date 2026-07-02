055ed37ad89a2eb3e2d1b1aa9fd1d48afa15baa6
Khaled Nabil <153304420+KhaledNabil10@users.noreply.github.com>
2026-07-02 09:33:30 +0300

    Add 197_rising_temperature.sql

--- SQL_50/197_rising_temperature.sql
+++ SQL_50/197_rising_temperature.sql
@@ -1,9 +1,9 @@
 -- Problem: 197. Rising Temperature
 -- Difficulty: Easy
--- https://leetcode.com/problems/rising-temperature/
+-- Link: https://leetcode.com/problems/rising-temperature/
 
 SELECT w1.id
 FROM Weather w1
 JOIN Weather w2
     ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
-    WHERE w1.temperature > w2.temperature;
+WHERE w1.temperature > w2.temperature;
